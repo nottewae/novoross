701c3e5788b85327c26ff1fd32335d061d4990ac
nottewae <nottewae@gmail.com>
2014-07-30 10:17:02 +0400

    dgh

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,8 +2,7 @@ class BlogController < ApplicationController
   def index
     if session[:admin]
       @admin=Admin.find session[:admin]
-    else
-      redirect_to root_path
+
     end
     @blog=Blog.new
     @blogs=Blog.all.order('created_at DESC')
