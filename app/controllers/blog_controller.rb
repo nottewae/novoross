3a9ccc13fa0ecd8bbe3119b8696e967057c26cab
nottewae <nottewae@gmail.com>
2014-09-08 14:13:47 +0400

    photo fix

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,7 +2,6 @@ class BlogController < ApplicationController
   def index
     if session[:admin]
       @admin=Admin.find session[:admin]
-
     end
     @blog=Blog.new
     @blogs=Blog.all.order('created_at DESC')
