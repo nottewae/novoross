b8d24b00341129c1d08455ccee3d251754cc85bd
nottewae <nottewae@gmail.com>
2014-10-09 22:03:02 +0400

    unicorn

--- restart_unicorn.rb
+++ restart_unicorn.rb
@@ -1,12 +1,15 @@
+
+Dir.chdir File.dirname(__FILE__)
+p "assets precompile"
+system "rake assets:precompile"
+p "database prepare"
+system "rake db:migrate"
 if File.exist?(File.join(File.dirname(__FILE__),"tmp","pids","unicorn.pid"))
   pid=File.open(File.join(File.dirname(__FILE__),"tmp","pids","unicorn.pid")).read
+  p "found pid:#{pid.to_i}"
   system "kill "+pid if pid && pid.to_i!=0
+  p "kill it"
 end
-Dir.chdir File.dirname(__FILE__)
-system "rake assets:precompile"
-system "rake db:migrate"
-
-
-
-
-system "unicorn_rails -c config/unicorn.conf.rb -E production -D"
+p "start unicorn"
+system "unicorn -c config/unicorn.conf.rb -E production -D"
+p "exit"
