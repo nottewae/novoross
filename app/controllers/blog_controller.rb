057503fcd918e217a4846e22fbc9a3f08fc00384
nottewae <nottewae@gmail.com>
2014-11-24 17:44:23 +0300

    dfghj

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -16,17 +16,29 @@ class BlogController < ApplicationController
     @contacts=Contact.all.order('created_at DESC')
   end
   def create_contact
+    if session[:admin]
+      @admin=Admin.find session[:admin]
+
     @contact=Contact.new contact_params
     if @contact.save
       render json:{result:true,data:@contact}
     else
       render json:{result:false,data:@contact.errors}
     end
+    else
+      render json:{result:"OOOps!"}
+    end
   end
   def destroy_contact
+    if session[:admin]
+      @admin=Admin.find session[:admin]
+
     @contact=Contact.find params[:id]
     @contact.destroy
     render json:{result:true}
+    else
+      render json:{result:"OOOps!"}
+    end
   end
   private
   def contact_params
